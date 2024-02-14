9d46d01ca970ac2877e9182dd9e28e3ee759b054
Said Mohammed <zeydmohammed1999@gmail.com>
2024-02-14 17:01:54 +0530

    white-rabit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('#main-banner').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    dots: false,
+    autoplay: true,
+    nav: false,
+    items: 1,
+    autoplayTimeout: 3000,
+    slideSpeed: 8000,
+    // onInitialized: customNavigation
+})
